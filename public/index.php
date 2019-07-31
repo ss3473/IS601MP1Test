36f99384407d43a5e83b9a23b59678cf3e381a66
sagar <ss3473@njit.edu>
2019-07-31 00:52:04 -0400

    Feature: Using a factory (in progress) Lambo bling wagon indicated in array.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-echo "test1";
-echo "Hello World";
-
 main:: start();
+
 class main
 {
     static public function start()
@@ -11,28 +9,66 @@ class main
         $records = csv::getRecords();
         $table = html::generateTable($records);
         system::printPage($table);
-        echo 'test';
+
     }
 }
-class csv {
+
+class csv
+{
 
     static public function getRecords()
     {
-        $records = 'test';
+        $make = 'lambo';
+        $model = 'Bling Wagon';
+        $car = AutomobileFactory::create($make, $model);
+
+        $records[] = $car;
+
+        print_r($records);
+
         return $records;
     }
 }
-class html{
+
+class html
+{
     static public function generateTable($records)
     {
         $table = $records;
         return $table;
     }
 }
-class system{
+
+class system
+{
 
     static public function printPage($page)
     {
         echo $page;
+    }
+}
+
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
     }
 }
