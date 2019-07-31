784d7d7a8e4d47767224ee7b6a6ceb714e1348a2
sagar <ss3473@njit.edu>
2019-07-30 23:24:02 -0400

    Edit: Added code in index.php to include class: csv,html,system

--- public/index.php
+++ public/index.php
@@ -1,4 +1,35 @@
 <?php
 
 echo "test1";
-echo "Hello World
+echo "Hello World";
+
+main:: start();
+class main
+{
+    static public function start()
+    {
+        $records = csv::getRecords();
+        echo 'test';
+    }
+}
+class csv {
+
+    static public function getRecords()
+    {
+        echo 'test';
+    }
+}
+class html{
+    static public function generateTable($records)
+    {
+        $table = $records;
+        return $table;
+    }
+}
+class system{
+
+    static public function printPage($page)
+    {
+        echo $page;
+    }
+}
