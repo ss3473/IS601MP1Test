c92ecac27b2717b30f73b5fed16c7da52d6dde3f
sagar <ss3473@njit.edu>
2019-07-31 10:22:58 -0400

    Feature: added sagar.csv file and updated index.php code for reference.

--- public/index.php
+++ public/index.php
@@ -1,74 +1,29 @@
 <?php
 
-main:: start();
+main::start("sagar.csv");
 
 class main
 {
-    static public function start()
+    static public function start($filename)
     {
-        $records = csv::getRecords();
-        $table = html::generateTable($records);
-        system::printPage($table);
+       $records = csv::getRecords($filename);
 
+       print_r($records);
     }
 }
 
-class csv
-{
+class csv {
 
-    static public function getRecords()
-    {
-        $make = 'lambo';
-        $model = 'Bling Wagon';
-        $car = AutomobileFactory::create($make, $model);
+    static public function getRecords($filename) {
 
-        $records[] = $car;
+        $file = fopen("sagar.csv", "r");
 
-        print_r($records);
+        while (!feof($file)) {
+            $record = fgetcsv($file);
 
-        return $records;
-    }
-}
+            $records[] = $record;
+        }
 
-class html
-{
-    static public function generateTable($records)
-    {
-        $table = $records;
-        return $table;
-    }
-}
-
-class system
-{
-
-    static public function printPage($page)
-    {
-        echo $page;
-    }
-}
-
-class Automobile
-{
-    private $vehicleMake;
-    private $vehicleModel;
-
-    public function __construct($make, $model)
-    {
-        $this->vehicleMake = $make;
-        $this->vehicleModel = $model;
-    }
-
-    public function getMakeAndModel()
-    {
-        return $this->vehicleMake . ' ' . $this->vehicleModel;
-    }
-}
-
-class AutomobileFactory
-{
-    public static function create($make, $model)
-    {
-        return new Automobile($make, $model);
-    }
+        fclose($file);
+        print_r($records);}
 }
