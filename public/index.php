57210511311b8cf723a64d5170e9744c9331032d
sagar <ss3473@njit.edu>
2019-08-01 01:01:50 -0400

    Edit: Added html class and modified code.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 main::start("sagar.csv");
@@ -8,36 +9,78 @@ class main
     {
        $records = csv::getRecords($filename);
 
-       $record = recordFactory::create();
+       $table = html::generateTable($records);
 
-       print_r($record);
+
     }
 }
 
+class html {
+
+
+    public static function generateTable($records) {
+
+        foreach ($records as $record) {
+            $array = $record->returnArray();
+            print_r($array);
+        }
+    }
+}
 class csv {
 
     static public function getRecords($filename) {
 
         $file = fopen("sagar.csv", "r");
 
+        $fieldNames = array();
+        $count = 0;
+
+
         while (!feof($file)) {
             $record = fgetcsv($file);
-
-            $records[] = $record;
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
         }
 
         fclose($file);
-        print_r($records);}
+        print_r($record);}
 }
 
-class record {}
+class record {
+
+    public function __construct(Array $fieldNames = null, $values = null)
+    {
+        $record = array_combine($fieldNames, $values);
+
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+
+        print_r($this);
+
+
+    }
+
+    public function createProperty($name = 'first', $value = 'name') {
+
+        $this->{$name} = $value;
+
+
+    }
+}
 
 class recordFactory {
 
-    public static function create(Array $array = null) {
+    public static function create(Array $fieldNames = null, Array $values = null) {
 
-        $record = new record();
+
+        $record = new record($fieldNames, $values);
 
         return $record;
     }
 }
+
